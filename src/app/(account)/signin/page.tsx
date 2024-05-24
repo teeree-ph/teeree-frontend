dbc8efc1d139c77732eedbf31886006a12176018
Ael <aelzimmer@gmail.com>
2024-05-24 08:08:42 +0800

    feat: Login page UI

--- src/app/(account)/signin/page.tsx
+++ src/app/(account)/signin/page.tsx
@@ -9,22 +9,24 @@ import {z} from "zod";
 import {useToast} from "@/components/ui/use-toast";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
+import Link from "next/link";
+import { useState } from "react";
 
 const formSchema = z.object({
-  username: z.string().min(1).trim(),
-  password: z.string().min(5).max(70).trim()
+  email: z.string().trim().min(1, { message: "请输入邮箱" }).email("邮箱格式不正确.").refine((e) => e.endsWith("@shphschool.com"), "请使用平和邮箱"),
+  password: z.string().min(5, { message: "请输入密码" }).max(70).trim()
 })
 
 export default function SignInPage() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      email: "",
       password: ""
     },
   })
   const { toast } = useToast()
-  let isDisabled = false
+  const [isDisabled, setIsDisabled] = useState(false)
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
@@ -35,20 +37,20 @@ export default function SignInPage() {
       <div className="flex justify-items-center items-center w-full h-full">
         <Card className="w-[350px] lg:w-[400px]">
           <CardHeader>
-            <CardTitle>Login</CardTitle>
-            <CardDescription>Sign in through your IronPulse ID</CardDescription>
+            <CardTitle>欢迎</CardTitle>
+            <CardDescription>登录你的账户</CardDescription>
           </CardHeader>
           <CardContent>
             <Form {...form}>
               <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
                 <FormField
                   control={form.control}
-                  name="username"
+                  name="email"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Username</FormLabel>
+                      <FormLabel>邮箱</FormLabel>
                       <FormControl>
-                        <Input placeholder="Enter your username" {...field} />
+                        <Input placeholder="@shphschool.com" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -59,18 +61,25 @@ export default function SignInPage() {
                   name="password"
                   render={({ field }) => (
                     <FormItem>
-                      <FormLabel>Password</FormLabel>
+                      <FormLabel>密码</FormLabel>
                       <FormControl>
-                        <Input type="password" placeholder="Enter your password" {...field} />
+                        <Input type="password" placeholder="" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
-                <Button type="submit" className="w-full" disabled={isDisabled}>
-                  {isDisabled ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
-                  Login
-                </Button>
+                <div className="flex justify-end">
+                  <Button variant="link" asChild>
+                    <Link href="/signup">
+                      还没有账号？
+                    </Link>
+                  </Button>
+                  <Button type="submit" disabled={isDisabled}>
+                    {isDisabled ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+                    登录
+                  </Button>
+                </div>
               </form>
             </Form>
           </CardContent>
